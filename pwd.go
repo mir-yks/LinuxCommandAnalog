68ef9630233429472759a427498b32ef704a45e4
Максим Мудров <mudrov010203@gmail.com>
2026-01-12 12:15:14 +0300

    pwd

--- pwd.go
+++ pwd.go
@@ -1,30 +1,86 @@
 package main
 
 import (
- "flag"
- "fmt"
- "os"
+    "flag"
+    "fmt"
+    "os"
+    "path/filepath"
 )
 
-func printCurrentDir() (string, error) {
- return os.Getwd()
+// Структура для хранения аргументов команды
+type PwdOptions struct {
+    Help        bool   
+    Logical     bool   
+    Physical    bool   
+    useLogical  bool   
+}
+
+// Функция для получения текущей директории с учетом флагов
+func getCurrentDir(options PwdOptions) (string, error) {
+    if options.useLogical {
+        if pwd := os.Getenv("PWD"); pwd != "" {
+            if _, err := os.Stat(pwd); err == nil && filepath.IsAbs(pwd) {
+                return pwd, nil
+            }
+        } 
+    }
+    
+    return os.Getwd()
+}
+
+// Функция для проверки и обработки флагов
+func parseFlags() PwdOptions {
+    var options PwdOptions
+    
+    flag.BoolVar(&options.Help, "h", false, "показать справку об использовании")
+    flag.BoolVar(&options.Help, "help", false, "показать справку об использовании")
+    flag.BoolVar(&options.Logical, "L", false, "использовать логический путь (разрешить символьные ссылки)")
+    flag.BoolVar(&options.Physical, "P", false, "использовать физический путь (без символьных ссылок)")
+    
+    flag.Parse()
+    
+    if flag.NArg() > 0 {
+        fmt.Fprintf(os.Stderr, "pwd: лишний операнд '%s'\n", flag.Arg(0))
+        fmt.Fprintf(os.Stderr, "Используйте 'pwd -h' для получения справки\n")
+        os.Exit(1)
+    }
+    
+    options.useLogical = options.Logical && !options.Physical
+    
+    return options
+}
+
+// Функция для отображения справки
+func showHelp() {
+    fmt.Println("Использование: pwd [КЛЮЧ]...")
+    fmt.Println("Выводит полное имя текущей рабочей директории.")
+    fmt.Println()
+    fmt.Println("Ключи:")
+    fmt.Println("  -L             использовать значение PWD из окружения, даже если он содержит символьные ссылки")
+    fmt.Println("  -P             использовать физическую структуру каталогов (без символьных ссылок)")
+    fmt.Println("  -h, --help     показать эту справку и выйти")
+    fmt.Println()
+    fmt.Println("Если не указано ни одного ключа, используется -P.")
+    fmt.Println()
+    fmt.Println("Примеры:")
+    fmt.Println("  pwd           # Вывести текущую директорию (эквивалентно pwd -P)")
+    fmt.Println("  pwd -L        # Использовать логический путь")
+    fmt.Println("  pwd -P        # Использовать физический путь")
 }
 
 func main() {
- help := flag.Bool("h", false, "показать помощь")
- flag.Parse()
-
- if *help {
-  fmt.Println("Использование: pwd [-h]")
-  fmt.Println("-h: помощь")
-  os.Exit(0)
- }
-
- currentDir, err := printCurrentDir()
- if err != nil {
-  fmt.Println("Ошибка:", err)
-  os.Exit(1)
- }
-
- fmt.Println(currentDir)
+    options := parseFlags()
+    
+    if options.Help {
+        showHelp()
+        os.Exit(0)
+    }
+    
+    currentDir, err := getCurrentDir(options)
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "pwd: ошибка получения текущей директории: %v\n", err)
+        os.Exit(1)
+    }
+    
+    fmt.Println(currentDir)
 }
