d71348e35f8a206d22555d42a85c1dad524eea6d
Максим Мудров <mudrov010203@gmail.com>
2026-01-12 11:36:01 +0300

    Команда exit

---
+++ exit.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"syscall"
+)
+
+func main() {
+	// Обработка ключа -help
+	help := flag.Bool("help", false, "Показать помощь и выйти")
+	flag.Parse()
+
+	if *help {
+		printHelp()
+		os.Exit(0)
+	}
+
+	shellPid :=os.Getppid() // получение PID терминала
+	syscall.Kill(shellPid, syscall.SIGHUP) 
+}
+
+func printHelp() {
+	fmt.Println("Использование: exit")
+	fmt.Println("Ключи:")
+	fmt.Println("  -help       Показать это сообщение и выйти")
+}
